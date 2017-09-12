2bef8b2b4183bbceeb92ccd690ea462d0eb835a0
Deaken Wylie <deaken.wylie@gmail.com>
2017-09-12 15:29:27 -0500

    adding a cow

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,4 +4,8 @@ alias vi="/usr/bin/vim"
 alias mu="cd $HOME/mu"
 alias derp="cd $HOME/mu/derp"
 alias top="/usr/bin/htop"
+# Hey, shut up, I have only the one hand...
+alias curseofwar="/usr/games/curseofwar -dee -i4 -l2 -q1 -sp -T -W40"
+alias cow="/usr/games/curseofwar -dee -i4 -l2 -q1 -sp -T -W40"
+# ...plus, also, I am bad at this game, too.  :C
 
