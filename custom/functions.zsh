db10f0ef2a6a44c3c975ed8b71749f14717eed38
Deaken Wylie <deaken.wylie@gmail.com>
2017-09-14 13:30:04 -0500

    added some aliases and functions

---
+++ custom/functions.zsh
@@ -0,0 +1,19 @@
+# dwylie - stolen from man plugin, changed shortcut keys.
+man-command-line() {
+  [[ -z $BUFFER ]] && zle up-history
+  [[ $BUFFER != man\ * ]] && LBUFFER="man $LBUFFER"
+}
+zle -N man-command-line
+# Defined shortcut keys: [Esc] m
+bindkey "\e"m man-command-line
+
+# dwylie - i can *almost* make this do what i want...
+# TODO: fix it, jeniouse~
+# echo-return-code() {
+#   echo
+  echo $?
+# }
+# zle -N echo-return-code
+# bindkey "\e"/ echo-return-code
+# bindkey "\e"\? echo-return-code
+
