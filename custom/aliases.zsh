9e3bdff7cd0fc12d0ad95009d0dee3966341fab4
Deaken Wylie <deaken.wylie@gmail.com>
2017-09-14 19:15:59 -0500

    better comments

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,19 +4,22 @@ alias vi='/usr/bin/vim'
 alias mu="cd $HOME/mu"
 alias derp="cd $HOME/mu/derp"
 alias top='/usr/bin/htop'
+alias lynx='/usr/bin/w3m'
 
 # Hey, shut up, I have only the one hand...
 alias curseofwar='/usr/games/curseofwar -dee -i4 -l2 -q1 -sp -T -W40'
 alias cow='/usr/games/curseofwar -dee -i4 -l2 -q1 -sp -T -W40'
 # ...plus, also, I am bad at this game, too.  :C
 
-alias -g H='| head'                                                                                                                 
-alias -g T='| tail'                                                                                                                 
-alias -g G='| grep'                                                                                                                 
-alias -g L="| less"
+# Stolen from the common-aliases plugin.
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L='| less'
 alias zshrc='/usr/bin/vim ~/.zshrc'
 # TODO: Steal more stuff from the end of the common-aliases plugin.
 
+# Inspired by / stolen from the perms plugin.
 alias 600='chmod 600'
 alias 644='chmod 644'
 alias 700='chmod 700'
